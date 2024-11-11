da7fb93e5174f67f1456c0c06a6a28d78c2515c9
No-cmoi <arno.muller@free.fr>
2024-11-11 16:02:09 +0100

    Création de plusieurs composants

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,8 @@ import { FaceSnap } from './models/face-snap';
 })
 export class AppComponent implements OnInit {
   mySnap!: FaceSnap;
+  myOtherSnap!: FaceSnap;
+  myLastSnap!: FaceSnap;
 
   ngOnInit(): void {
     this.mySnap = new FaceSnap(
@@ -21,6 +23,22 @@ export class AppComponent implements OnInit {
       'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
       new Date(),
       10
+    );
+
+    this.myOtherSnap = new FaceSnap(
+      'Sydney Opera House',
+      'Le bâtiment emblématique de la ville de Sydney en Australie',
+      'https://upload.wikimedia.org/wikipedia/commons/7/7c/Sydney_Opera_House_-_Dec_2008.jpg',
+      new Date(),
+      15
+    );
+
+    this.myLastSnap = new FaceSnap(
+      'Three Sisters',
+      'Les célèbres rochers des Blue Mountains de l\'état du NSW',
+      'https://upload.wikimedia.org/wikipedia/commons/e/e4/Blue_mountains_-_three_sisters.jpg',
+      new Date(),
+      25
     );
   }
 }
